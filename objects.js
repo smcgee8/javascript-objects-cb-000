8e879293065aba1e5303868c6bd8f80398406b24
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 14:04:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,4 @@
-playlist = {kanye: "song1", chance: "song2"
-}
+playlist = {kanye: "song1", chance: "song2"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
