d458f1b245c0c0b38a8bd89b0c59aa5feddac7ef
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-17 14:06:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {kanye: "song1", chance: "song2"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
-  return playlist
+  return playlist;
 }
